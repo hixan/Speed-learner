9dc8d3bcfb92bed1126497d6c164b0b793ed2aaf
Hixan <alex.88.elias@gmail.com>
2019-05-29 18:44:16 +1000

    cleaned up code, renamed functions appropriately

--- read_data.py
+++ read_data.py
@@ -1,23 +1,15 @@
 import cv2
 import pynmea2
 from collections import namedtuple
-import pandas as pd
 import datetime
-import textwrap
 
 # set up path variables
-mp4s = []
-with open('files.txt', 'r') as read:
-    for rpath in read.readlines():
-        path = rpath.strip()
-        if path[-3:].upper() == 'MP4':
-            mp4s.append(path)
-
 
 GSENSORD = namedtuple('GSENSORD', [*'xyz'])
 GPSDC = namedtuple('GPSDC', ())
 
 
+# {{{
 def parse_nmea_line(line):  # handle sentances pynmea2 cant handle
     start, *rest = line.strip().split(',')
     if start == '$GSENSORD':  # g-sensor data
@@ -25,12 +17,25 @@ def parse_nmea_line(line):  # handle sentances pynmea2 cant handle
     elif start == '$GPSDC':  # gps disconnected as far as i can tell
         return GPSDC()
     return pynmea2.parse(line)
+# }}}
 
 
-def read_file(video_path, sample_function=None, read_nmea=True,
-              gps_assignment='floor'):
-    '''video_path like /media/user/device/.../file_identifier.mp4
-    sample_function returns true if frame number is to be included in sample'''
+# {{{
+def read_nmea_file(filename):
+    with open(filename, 'r') as nmea_file:
+        return [parse_nmea_line(line) for line in nmea_file]
+# }}}
+
+
+# {{{
+def read_video_file(video_path, sample_function=None):
+    ''' generator for video frame data from a file
+    :param video_path: string like /media/user/device/.../YYMMDD-hhmmss.mp4
+        specifies video file location and filename.
+    :param sample_function: returns true if frame number is to be included in
+    sample
+    :return:
+    '''
 
     print(f'reading {video_path}')
     *path, last = video_path.split('/')
@@ -39,25 +44,19 @@ def read_file(video_path, sample_function=None, read_nmea=True,
     # formatted FILEyymmdd-hhmmss.extension
     starting_timestamp = datetime.datetime.strptime(last.strip(),
                                                     'FILE%y%m%d-%H%M%S.MP4')
-    # handle nmea if necessary
-    if read_nmea:
-        nmea_path = video_path[:-4] + '.NMEA'  # todo use actual pathlib
-        with open(nmea_path, 'r') as nmea_file:
-            nmea_data = [parse_nmea_line(line) for line in nmea_file]
-    else:
-        nmea_data = None
-
     cap = cv2.VideoCapture(video_path)
-    # frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # default sample_function
     if sample_function is None:
         def sample_function(n):
             return True
+
     frame_number = 0
     ret = cap.isOpened()  # only loop if capture object opened correctly
     while ret:  # keep reading until frame doesnt exist
         frame_number += 1
 
-        ret, frame = cap.read() # step through to next frame
+        ret, frame = cap.read()  # step through to next frame
 
         # yield data if contained in sample
         if sample_function(frame_number):
@@ -68,8 +67,16 @@ def read_file(video_path, sample_function=None, read_nmea=True,
             timestamp = starting_timestamp + \
                 datetime.timedelta(milliseconds=relative_timestamp)
 
-            yield frame_number, frame, timestamp
+            yield frame, timestamp
+# }}}
 
 
-for fn, fr, msec in read_file(mp4s[0], lambda x: x % 1 == 0):
-    print(msec)
+if __name__ == '__main__':
+    mp4s = []
+    with open('files.txt', 'r') as read:
+        for rpath in read.readlines():
+            path = rpath.strip()
+            if path[-3:].upper() == 'MP4':
+                mp4s.append(path)
+    for fr, msec in read_video_file(mp4s[0], lambda x: x % 1 == 0):
+        print(msec, end='\r')
