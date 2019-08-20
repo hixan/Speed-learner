9cabcb29fa933a8ffdea0d52edb4bed4cea8bf58
Hixan <alex.88.elias@gmail.com>
2019-08-20 13:59:06 +1000

    added NmeaFile object

--- read_data.py
+++ read_data.py
@@ -1,10 +1,11 @@
 import cv2
 import pynmea2
 from collections import namedtuple
-import datetime
+from datetime import datetime, timedelta
 from transformations import settings
+import pandas as pd
+from pathlib import Path
 
-# set up path variables
 
 GSENSORD = namedtuple('GSENSORD', [*'xyz'])
 GPSDC = namedtuple('GPSDC', ())
@@ -22,7 +23,67 @@ def parse_nmea_line(line):  # handle sentances pynmea2 cant handle {{{
 
 def read_nmea_file(filename):  # {{{
     with open(filename, 'r') as nmea_file:
-        return [parse_nmea_line(line) for line in nmea_file]
+        return tuple(parse_nmea_line(line) for line in nmea_file)
+
+
+# }}}
+
+class NmeaFile(pd.DataFrame):  # {{{
+    # reads nmea file and adjusts timestamps to correspond to video timestamps
+    #
+    # useful nmea sentances:
+    # GSENSORD - gsensor data - 3 values correspond to x, y, z, TODO check
+    # order
+
+    # GPRMC - position velocity time. contains timestamp, latitude, longitude,
+    #   speed in knots, track angle in degrees, date
+
+    # GPGGA - fix data includes altitude.
+
+    # not useful:
+    # GPGSA - sattelite info
+    # GPGSV - sattelite info
+
+    def __init__(self, filepath: Path,
+                 timestampformat: str = 'FILE%y%m%d-%H%M%S.NMEA'):
+
+        columns = ('tstamp', 'latitude', 'longitude', 'sense_x', 'sense_y',
+                   'sense_z', 'speed', 'direction', 'video_timestamp',
+                   'video_file')
+
+        parsed = read_nmea_file(str(filepath))
+        # throw away gpgsv data which happens to be at positions n, n+4, n+5,
+        # n+6, n+7 and group other types (gsensor, rmc, gga)
+        observations = [parsed[x+1:x+4] for x in range(0, len(parsed), 8)]
+
+        starting_timestamp = None  # will store the first timestamp in series
+        vals = {c: [] for c in columns}
+
+        # add observations to the dataframe
+        for gsense, rmc, gga in observations:
+            tstamp = datetime.combine(rmc.datestamp, rmc.timestamp)
+            speed = rmc.spd_over_grnd * 1.852  # speed in km, originally knots
+            direction = rmc.true_course  # direction in degrees from N
+            if starting_timestamp is None:
+                starting_timestamp = tstamp
+            vtimestamp = tstamp - starting_timestamp
+            lat = (-1 if rmc.lat_dir == 'W' else 1) * rmc.lat
+            lon = (-1 if rmc.lon_dir == 'S' else 1) * rmc.lon
+
+            # TODO - categorical video file?
+            vals['tstamp'].append(tstamp)
+            vals['speed'].append(speed)
+            vals['longitude'].append(lon)
+            vals['latitude'].append(lat)
+            vals['direction'].append(direction)
+            vals['sense_x'].append(gsense.x)
+            vals['sense_y'].append(gsense.y)
+            vals['sense_z'].append(gsense.z)
+            vals['video_timestamp'].append(vtimestamp)
+            vals['video_file'].append(filepath.stem + '.MP4')
+
+            super().__init__(vals)
+
 # }}}
 
 
@@ -42,7 +103,7 @@ class VideoReader:  # {{{
         self.video_path = video_path
         self.cap = cv2.VideoCapture(video_path)
         if starting_timestamp is None:
-            self.starting_timestamp = datetime.datetime.strptime(
+            self.starting_timestamp = datetime.strptime(
                 last.strip(),
                 'FILE%y%m%d-%H%M%S.MP4'
             )
@@ -68,7 +129,7 @@ class VideoReader:  # {{{
         # calculate absolute timestamp
         relative_timestamp = self.cap.get(cv2.CAP_PROP_POS_MSEC)
         timestamp = self.starting_timestamp + \
-            datetime.timedelta(milliseconds=relative_timestamp)
+            timedelta(milliseconds=relative_timestamp)
 
         self.ret, frame = self.cap.read()  # step through to next frame
 
@@ -94,12 +155,4 @@ class VideoReader:  # {{{
 
 
 if __name__ == '__main__':
-    mp4s = []
-    with open('files.txt', 'r') as read:
-        for rpath in read.readlines():
-            path = rpath.strip()
-            if path[-3:].upper() == 'MP4':
-                mp4s.append(path)
-    gen = VideoReader(mp4s[0])
-    for fr, msec in gen:
-        print(msec, end='\r')
+    print(NmeaFile(Path('example_data/FILE180603-225817.NMEA')).describe())
