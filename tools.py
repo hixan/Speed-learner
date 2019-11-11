db7ab50235718baf597a0c6ab040ea766d02c949
Hixan <alex.88.elias@gmail.com>
2019-11-11 14:55:47 +1100

    Created new branch for refactoring and project restructuring.

--- tools.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-from read_data import NmeaFile
-
-
-def open_gmaps(lon, lat):
-    lon = NmeaFile._dd_to_dms(lon, ('E', 'W'))
-    lat = NmeaFile._dd_to_dms(lat, ('N', 'S'))
-    link = "www.google.com/maps/place/{}%C2%B0{}'{:.1f}%22{}+{}%C2%B0{}'{:.1f}%22{}".format(*lon, *lat)
-    webbrowser.open_new_tab(link)
