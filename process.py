9093f12bd788886b9cfbca7999bb74e79b01b2a6
Hixan <alex.88.elias@gmail.com>
2019-11-17 15:26:58 +1100

    Added logging and optimised the method of data access and storage.

--- process.py
+++ process.py
@@ -5,15 +5,50 @@ from glob import glob
 from random import sample
 from tqdm import tqdm
 from typing import List
+import logging
+import json
 
-# Script variables
-DATADIR = Path('data')
-FILE_COUNT = 2  # number of files to sample. None selects all files
+logging.INFOFRAME = 21
+logging.INFOFILE = 22
+logging.addLevelName(logging.INFOFRAME, 'INFOFRAME')
+logging.addLevelName(logging.INFOFILE, 'INFOFILE')
 
-sample_files : List[Path] = list(map(Path, glob(f'{DATADIR}/*/*.MP4')))
-if FILE_COUNT is not None:
-    sample_files = sample(sample_files, FILE_COUNT)
+logger = logging.getLogger('process')
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler('process.log', 'w')
+fh.setLevel(logging.INFOFRAME)
+fmtstr = '%(name)s:%(levelname)s: %(asctime)s \n%(message)s'
+fh.setFormatter(logging.Formatter(fmtstr))
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter(fmtstr.replace('\n', '')))
+ch.setLevel(logging.INFOFILE)
+logger.addHandler(fh)
+logger.addHandler(ch)
 
-for f in tqdm(sample_files):
-    print(f)
-    generate_training_data(f, Path('./processed_data'), 20, np.array((0, -1, -2, -3)), .3)
+if __name__ == '__main__':
+
+    generate_trainset = True
+
+    if generate_trainset:
+        log = logger.getChild('generate_trainset')
+        # Script variables
+        DATADIR = Path('data')
+        FILE_COUNT = 2  # number of files to sample. None selects all files
+        OUTPUT_DIR = Path('./processed_data/frame_prediction/')
+
+        sample_files : List[Path] = list(map(Path, glob(f'{DATADIR}/*/*.MP4')))
+        if FILE_COUNT is not None:
+            sample_files = sample(sample_files, FILE_COUNT)
+
+        meta = []
+        for f in tqdm(sample_files, desc='files'):
+            meta.extend(generate_training_data(
+                file=f,
+                output_dir=OUTPUT_DIR,
+                sample_size=5,
+                relative_chain=np.array((0, -2, -4, -6)),
+                rescale=.3,
+                logger=log
+            ))
+        with open(OUTPUT_DIR / 'meta.json', 'w') as f:
+            print(json.dumps(meta), file=f)
--- processing/frame_prediction/generate_train.py
+++ processing/frame_prediction/generate_train.py
@@ -7,31 +7,35 @@ from pathlib import Path
 from typing import List
 from tqdm import tqdm
 from operator import or_
+from typing import Tuple, Union
+import logging
 
-def generate_training_data(file: Path, output_dir: Path, sample_size: int, relative_chain, rescale: int, timefmt: str = '%y%m%d-%H%M%S%f'):
+
+def generate_training_data(file: Path, output_dir: Path, sample_size: int, relative_chain: Tuple[int, ...], rescale: int = .3, timefmt: str = '%y%m%d-%H%M%S%f', logger: Union[logging.Logger, None] = None):
+
     vid = VideoReader(file)
     next(vid)
     framegroups = list(map(lambda x: list(relative_chain + x), sorted(sample(range(-min(relative_chain), len(vid)-max(relative_chain)), sample_size))))
-    framegroups = [[1,2,3],[2,3,4]]
-    print(framegroups)
     needed_frames = reduce(or_, map(set, framegroups), set())
     saved_frames = {}
-    for i, (frame, timestamp) in enumerate(vid):
+
+    for i, (frame, timestamp) in tqdm(enumerate(vid), total=max(needed_frames), desc='frames'):
         if i in needed_frames:
+
+            frame_filename = output_dir / 'frames' / timestamp.strftime(timefmt + '.jpg')
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frame = cv2.resize(frame, tuple((np.array(frame.shape) * rescale).astype('uint8')[::-1]))
-            print(f'added frame at {i}')
-            saved_frames[i] = frame, timestamp
+            scaled_size = (np.array(frame.shape[::-1]) * rescale).astype('int64')
+            frame = cv2.resize(frame, tuple(scaled_size))
+
+            logger.log(logging.INFOFRAME, f'added frame {i} from {vid.video_path} as {frame_filename}')
+
+            cv2.imwrite(str(frame_filename), frame)
+            saved_frames[i] = frame_filename.name
             needed_frames.remove(i)
             if len(needed_frames) == 0:
                 break
-
-    for framegroup in framegroups:
-        frames = [saved_frames[i] for i in framegroup]
-        groupname = frames[0][1].strftime(timefmt)
-        cv2.imwrite(str(output_dir / 'y' / (groupname + '.jpg')), frames[0][0])
-        for frame in frames[1:]:
-            cv2.imwrite(str(output_dir / 'x' / (groupname + '_' + f'{frame[1].strftime(timefmt)}.jpg')), frame[0])
-
-
-
+    logger.log(logging.INFOFILE, f'processed {len(saved_frames)} frames from {file}.')
+    output_meta = [{'y': saved_frames[y], 'x': list(map(lambda b: saved_frames[b], x))} for y, *x in framegroups]
+    logger.log(logging.INFOFILE, f'finished processing {file}.')
+    return output_meta
