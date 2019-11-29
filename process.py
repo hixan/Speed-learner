a073456986e290556024d9ea3afd83a65cd3186b
Hixan <alex.88.elias@gmail.com>
2019-11-29 12:13:18 +1100

    cleaned up process.py and added pre-generated file checking.

--- process.py
+++ process.py
@@ -1,4 +1,9 @@
 from processing.frame_prediction.generate_train import generate_training_data
+from pyspark import SparkContext
+import torch
+from processing.read_data import VideoReader
+from processing.frame_prediction.naive_net import Naive
+from processing.frame_prediction.train_naive import main as train_main
 import numpy as np
 from pathlib import Path
 from glob import glob
@@ -7,48 +12,120 @@ from tqdm import tqdm
 from typing import List
 import logging
 import json
+import cv2
+import time
 
-logging.INFOFRAME = 21
-logging.INFOFILE = 22
-logging.addLevelName(logging.INFOFRAME, 'INFOFRAME')
-logging.addLevelName(logging.INFOFILE, 'INFOFILE')
+def init_logging():
+    logging.INFOFRAME = 21
+    logging.INFOFILE = 22
+    logging.addLevelName(logging.INFOFRAME, 'INFOFRAME')
+    logging.addLevelName(logging.INFOFILE, 'INFOFILE')
 
-logger = logging.getLogger('process')
-logger.setLevel(logging.INFO)
-fh = logging.FileHandler('process.log', 'w')
-fh.setLevel(logging.INFOFRAME)
-fmtstr = '%(name)s:%(levelname)s: %(asctime)s \n%(message)s'
-fh.setFormatter(logging.Formatter(fmtstr))
-ch = logging.StreamHandler()
-ch.setFormatter(logging.Formatter(fmtstr.replace('\n', '')))
-ch.setLevel(logging.INFOFILE)
-logger.addHandler(fh)
-logger.addHandler(ch)
+    logger = logging.getLogger('process')
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler('process.log')
+    fh.setLevel(logging.DEBUG)
+    fmtstr = '%(name)s:%(levelname)s: %(asctime)s \n%(message)s'
+    fh.setFormatter(logging.Formatter(fmtstr))
+    ch = logging.StreamHandler()
+    ch.setFormatter(logging.Formatter(fmtstr.replace('\n', '')))
+    ch.setLevel(logging.INFOFILE)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+
+    return logger
+
+
+def test_naive():
+    EXAMPLE = Path(
+        './processed_data/frame_prediction/frames/170523-173539271876.jpg'
+    )
+    EXAMPLE2 = Path(
+        './processed_data/frame_prediction/frames/170523-173539305301.jpg'
+    )
+
+    im = cv2.imread(str(EXAMPLE))[:,:,1]
+    im2 = cv2.imread(str(EXAMPLE2))[:,:,1]
+
+    im_pytorch = torch.Tensor((im, im2)).reshape(1, 2, *im.shape)
+    logger.log(logging.INFO, f'image dimensions: {im.shape}')
+
+    net = Naive(2, 1)
+    logger.log(logging.INFO, f'net initialized: {net}')
+    out = net(im_pytorch)[0][0].detach().numpy()
+    logger.log(logging.INFO, f'output_range: {np.min(out)}, {np.max(out)}')
+    out = out - np.min(out)
+    logger.log(logging.INFO, f'output_range: {np.min(out)}, {np.max(out)}')
+    out = out / np.max(out) * 255
+    out = out.astype('uint8')
+    logger.log(logging.INFO, f'output dimensions: {out.shape}')
+    logger.log(logging.INFO, f'output_range: {np.min(out)}, {np.max(out)}')
+
+    cv2.imshow('input', np.concatenate((im, im2), axis=0))
+    cv2.waitKey(0)
+    cv2.imshow('output', out)
+    cv2.waitKey(0)
+
+
+def generate_trainset():
+    log = logger.getChild('generate_trainset')
+
+    # Script variables
+    DATADIR = Path('data')
+    FILE_COUNT = None  # number of files to sample. None selects all files
+    OUTPUT_DIR = Path('./processed_data/frame_prediction/')
+
+    sample_files : List[Path] = list(map(Path, glob(f'{DATADIR}/*/*.MP4')))
+    if FILE_COUNT is not None:
+        sample_files = sample(sample_files, FILE_COUNT)
+
+    def reduce_meta_files(l1, l2):
+        args1 = l1['args']
+        args2 = l2['args']
+        fp = args1['filepath'] + '\t' + args2['filepath']
+        del args1['filepath']
+        del args2['filepath']
+
+        assert args1 == args2
+        args1['filepath'] = fp
+        l1['data'] += l2['data']
+        l1['args'] = args1
+        return l1
+
+    def process_video(filepath, check_processed=True):
+        print(filepath)
+        return generate_training_data(
+            filepath,
+            output_dir=OUTPUT_DIR,
+            average_sps=0.2,
+            relative_chain=(0,-2,-4,-8),
+            rescale=.3,
+            timefmt='%y%m%d-%H%M%S%f',
+            logger=log,
+            check_processed=check_processed,
+        )
+
+    sc = SparkContext()
+    video_files = sc.parallelize(sample_files)
+    meta_files = video_files.map(process_video)
+    with open(OUTPUT_DIR / f'all_out.json', 'w') as f:
+        json.dump(meta_files.reduce(reduce_meta_files), f)
+
+
+
+logger = init_logging()
 
 if __name__ == '__main__':
 
-    generate_trainset = True
+    function = 'generate_trainset'
 
-    if generate_trainset:
-        log = logger.getChild('generate_trainset')
-        # Script variables
-        DATADIR = Path('data')
-        FILE_COUNT = 2  # number of files to sample. None selects all files
-        OUTPUT_DIR = Path('./processed_data/frame_prediction/')
+    if function == 'train_naive':
+        train_main()
 
-        sample_files : List[Path] = list(map(Path, glob(f'{DATADIR}/*/*.MP4')))
-        if FILE_COUNT is not None:
-            sample_files = sample(sample_files, FILE_COUNT)
+    if function == 'test_naive':
+        test_naive()
 
-        meta = []
-        for f in tqdm(sample_files, desc='files'):
-            meta.extend(generate_training_data(
-                file=f,
-                output_dir=OUTPUT_DIR,
-                sample_size=5,
-                relative_chain=np.array((0, -2, -4, -6)),
-                rescale=.3,
-                logger=log
-            ))
-        with open(OUTPUT_DIR / 'meta.json', 'w') as f:
-            print(json.dumps(meta), file=f)
+    if function == 'generate_trainset':
+        generate_trainset()
+
+
--- processing/frame_prediction/generate_train.py
+++ processing/frame_prediction/generate_train.py
@@ -1,31 +1,55 @@
 from ..read_data import VideoReader
+import json
 from random import sample
 import numpy as np
 from functools import reduce
 import cv2
 from pathlib import Path
 from typing import List
-from tqdm import tqdm
 from operator import or_
 from typing import Tuple, Union
 import logging
 
-def generate_training_data(vid: Union[Path, VideoReader],
-                           output_dir: Path,
-                           average_sps: float,  # average samples per second
-                           relative_chain: Tuple[int, ...],
-                           rescale: int = .3,
-                           timefmt: str = '%y%m%d-%H%M%S%f',
-                           logger: Union[logging.Logger, None] = None):
-
+def generate_training_data(
+        vid: Union[Path, VideoReader],
+        output_dir: Path,
+        average_sps: float,  # average samples per second
+        relative_chain: Tuple[int, ...],
+        rescale: int = .3,
+        timefmt: str = '%y%m%d-%H%M%S%f',
+        logger: Union[logging.Logger, None] = None,
+        check_processed: bool = True
+):
     if logger is None:
         logger = logging.Logger()  # will log messages but wont output anywhere
     if type(vid) is not VideoReader:
-        vid : VideoReader = VideoReader(vid)
+        try:
+            vid : VideoReader = VideoReader(vid)
+        except OSError:
+            raise
+    args = {
+        'filepath': str(vid.video_path),
+        'output_dir': str(output_dir),
+        'average_sps': average_sps,
+        'relative_chain': list(relative_chain),
+        'rescale': rescale,
+        'timefmt': timefmt,
+    }
+    output_file = (
+        output_dir /
+        f'meta_{vid.starting_timestamp.strftime(timefmt)}.json'
+    )
+    if check_processed and output_file.exists():
+        # dont re-evaluate frames if computation is the same. Just load return
+        # value from previous run.
+        with open(output_file, 'r') as f:
+            meta_old = json.load(f)
+            if meta_old['args'] == args:
+                return meta_old
     next(vid)  # skip metadata
     sample_size = int(vid.frame_count / vid.fps * average_sps)
     framegroups = list(map(
-        lambda x: list(relative_chain + x),
+        lambda x: list(np.array(relative_chain) + x),
         sorted(sample(range(
             -min(relative_chain),
             len(vid)-max(relative_chain)
@@ -34,21 +58,19 @@ def generate_training_data(vid: Union[Path, VideoReader],
     needed_frames = reduce(or_, map(set, framegroups), set())
     saved_frames = {}
 
-    for i, (frame, timestamp) in tqdm(enumerate(vid),
-                                      total=max(needed_frames),
-                                      desc='frames'):
+    for i, (frame, timestamp) in enumerate(vid):
         if i in needed_frames:
             frame_filename = output_dir / 'frames' / timestamp.strftime(
-                '{timefmt}.jpg'
+                f'{timefmt}.bmp'
             )
 
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             scaled_size = (np.array(frame.shape[::-1]) *
-                           rescale).astype('int64')
+                        rescale).astype('uint8')
             frame = cv2.resize(frame, tuple(scaled_size))
 
             logger.log(
-                logging.INFOFRAME,
+                logging.INFO,
                 f'added frame {i} from {vid.video_path} as {frame_filename}'
             )
 
@@ -57,15 +79,17 @@ def generate_training_data(vid: Union[Path, VideoReader],
             needed_frames.remove(i)
             if len(needed_frames) == 0:
                 break
-    logger.log(logging.INFOFILE,
-               f'processed {len(saved_frames)} frames from {vid.video_path}.')
-    output_meta = [
-        {'y': saved_frames[y],
-         'x': list(map(lambda b: saved_frames[b], x))} for y, *x in framegroups
-    ]
-    logger.log(logging.INFOFILE, f'finished processing {vid.video_path}.')
-    with open(output_dir /
-              'meta_{vid.starting_timestamp.strftime(timefmt)}.json',
-              'w') as f:
+    logger.log(logging.INFO,
+            f'processed {len(saved_frames)} frames from {vid.video_path}.')
+    output_meta = {
+        'args': args,
+        'data': [
+            {'y': saved_frames[y], 'x': list(map(lambda b: saved_frames[b], x))}
+            for y, *x in framegroups
+        ]
+    }
+    logger.log(logging.INFO, f'finished processing {vid.video_path}.')
+
+    with open(output_file, 'w') as f:
         print(json.dumps(output_meta), file=f)
-    return True
+    return output_meta
